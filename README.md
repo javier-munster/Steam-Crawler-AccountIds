c93bd87cb69b7c97a7c112a2114bb1c254dd0b5e
Javier Munster <javier.munster@gmail.com>
2021-07-09 19:57:12 -0400

    Removed Redis and moved to a pure DynamoDB implementation for storage

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # SteamID Crawler
-Cralws Steam API for all user Steam IDs
+AWS Lambda function that cralws the Steam API for all gamers' Steam IDs, storing the data into DynamoDB. I use the ISteamUser GetPlayerSummaries endpoint because it allows me to search for 100 users per request. This is the most out of any endpoint in the Steam API.
 
 ## Steam Countries
-Steam country, state and city codes are specific to Steam and are not very human readable. I use the [data](https://github.com/Holek/steam-friends-countries) made available by <b>[@Holek](https://github.com/Holek)</b> to convert it into a more useful format.
+Steam country, state and city codes are specific to Steam and are not human readable. I use the [data](https://github.com/Holek/steam-friends-countries) made available by <b>[@Holek](https://github.com/Holek)</b> to convert it into a more useful format. The license for it available [here](./steam_countries_license.txt).
+
+## FAQ
+### Why is there almost no error handling?
+It was a concious decision not to catch errors or rejected promises in the code. Failures are better hangled through the AWS configuration than at the code level. Future crawler invocations will retry the same failed steamId range, ensuring no data loss. Our key in DynamoDB ensures no duplicate items are created. On failure, I am notified through AWS SNS and the error is logged in CloudWatch Logs.
+
+### Why do you store the Lambda state in the DB?
+We do have a state to store, but Lambda functions do not necessarily remember the state between invocations. The original implementation used a Redis instance in AWS ElastiCache to store the state between invocations. I changed this because it is easier for me to check the crawler's progress using the DynamoDB UI and because ElastiCache has a fixed monthly cost that is significantly higher than just using DynamoDB. The performance gain from Redis was not such a big concern.
+
+### Why are are some promises manually timed out?
+On a successful execution, this code can take a long time to finish executing. The maximum timeout that a Lambda function can be configured to is 15 minutes. I did not want to wait the full duration just because of an AWS configuration error, which are bound to happen.
+
+### What technologies are used by this crawler?
+- Node.js v14.16.1 for the code
+- JSDoc for documentation
+- AWS DynamoDB for data and state storage
+- ~~Redis for state storage~~ Not any more as of v2.0.0, see FAQ for reasons
+- AWS Lambda function for the environment
+- AWS SNS for failure notifications
+- AWS EventBridge for Lambda invocations
+- AWS VPC for internet access
+- AWS IAM for security policies
--- index.js
+++ index.js
@@ -1,88 +1,148 @@
 'use strict';
 const axios = require('axios');
-const steamUsers = require('./utils/steamUsers');
-const redisController = require('./utils/redisController');
+const AWS = require('aws-sdk');
 
+const steamCountries = require('./utils/steamCountries');
+
+/** @constant {string} */
 const STEAM_API_KEY = process.env.STEAM_API_KEY;
-const END_ACCOUNT_ID = process.env.END_ACCOUNT_ID || 1224000000;
-const BATCH_SIZE = 100;
-const INTER_REQUEST_DELAY = Number(process.env.INTER_REQUEST_DELAY) || 5000; // 864
-const REQUESTS_PER_INVOCATION = process.env.REQUESTS_PER_INVOCATION || 20;
+/** @constant {number} */
+const END_ACCOUNT_ID = process.env.END_ACCOUNT_ID || 1224000000; // Stops crawling at this accountId
+/** @constant {number} */
+const BATCH_SIZE = 100; // Number of steamIds to include in each request. Set by Steam API
+/** @constant {number} */
 const STEAM_REQUEST_TIMEOUT = process.env.STEAM_REQUEST_TIMEOUT || 10000;
+/** @constant {boolean} */
+const IS_LAMBDA_ENVIRONMENT = process.env.IS_LAMBDA_ENVIRONMENT || false;
+/** @constant {number} */
+const REQUESTS_PER_INVOCATION = process.env.REQUESTS_PER_INVOCATION || 2;
 
-let requestCount = 0;
+const DocClient = new AWS.DynamoDB.DocumentClient({
+    region: "us-east-1",
+    apiVersion: "2012-08-10"
+});
 
-function makeRequest(startAccountId, batchSize) {
-    console.log(`Processing steamids from ${generateSteamId(startAccountId)} to ${generateSteamId(startAccountId + batchSize - 1)}`);
-    let steamids = [];
+let requestCount = 0; // Keeps track of the number of requests made, used to evaluate when invocation limit is reached
+
+/**
+ * Requests for 100 steamIds from Steam's ISteamUser GetPlayerSummaries API
+ * @param  {number} startAccountId
+ * @param  {number} batchSize
+ * @returns {Promise}
+ */
+async function getSteamIds(startAccountId, batchSize) {
+    /** Generate steamIds from accountIds */
+    let steamIds = [];
 
     for (let i = 0; i < batchSize; i++) {
-        steamids.push(generateSteamId(startAccountId + i));
+        steamIds.push(generateSteamId(startAccountId + i));
     }
 
-    let requestTimeout;
+    console.log(`Processing steamIds from ${steamIds[0]} to ${steamIds[steamIds.length - 1]}`);
 
-    return Promise.race([axios.get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002", {
+    /** Steam API Request */
+    const steamResponse = await racePromise(axios.get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002", {
         params: {
             key: STEAM_API_KEY,
-            steamids: steamids.join(',')
+            steamids: steamIds.join(',')
         }
-    }), new Promise((resolve, reject) => {
-        // Axios will not timeout requests if there is no internet access, so it must be timed out manually
-        requestTimeout = setTimeout(() => {
-            console.warn("Steam request timeout!");
-            return reject("Steam request timeout");
-        }, STEAM_REQUEST_TIMEOUT);
-    })]).then((res) => {
-        if (!!requestTimeout) {
-            clearTimeout(requestTimeout);
-        }
+    }), STEAM_REQUEST_TIMEOUT, "getSteamIds");
 
-        if (res.status !== 200) {
-            console.log("Steam request failed with status:", res.status);
-            redisController.disconnect();
-            return Promise.reject(`Response failed with status: ${res.status}`);
-        }
-        // console.log("response", res.data.response.players);
-        console.log("response", res.status, res.data.response.players.length);
+    if (steamResponse.status !== 200) {
+        console.log("Steam request failed with status:", steamResponse.status);
+        return Promise.reject(`Response failed with status: ${steamResponse.status}`);
+    }
 
-        return steamUsers.batchWriteUsers(res.data.response.players);
-    });
-}
+    const users = steamResponse.data.response.players;
+    console.log("Steam API Response:", users.length);
 
-function wait(time) {
-    console.log("waiting...");
-    return new Promise((resolve, reject) => {
-        return setTimeout(() => {
-            return resolve();
-        }, time)
-    });
-}
+    let localAccountId = await getParameter("localAccountId");
 
-function processSequentially(steamid) {
-    return makeRequest(steamid, BATCH_SIZE).then(() => {
-        requestCount++;
-        if (requestCount < REQUESTS_PER_INVOCATION) {
-            return wait(INTER_REQUEST_DELAY).then(() => {
-                return processSequentially(steamid + BATCH_SIZE);
+    /** Map Steam response into DynamoDB request */
+    // DynamoDB batch write supports a max of 25 operations, so they must be batched
+    const ddbBatchWrites = [];
+
+    for (let i = 0; i < Math.ceil(users.length / 25); i++) {
+        ddbBatchWrites.push({
+            RequestItems: {
+                steamUsers: users.slice(i * 25, (i * 25) + 25).map((user) => {
+                    let Item = {
+                        steamId: user.steamid.toString(),
+                        localAccountId: localAccountId++
+                    };
+
+                    if (!!user.personastate) {
+                        Item.personaState = Number(user.personastate);
+                    }
+
+                    if (!!user.communityvisibilitystate) {
+                        Item.communityVisibilityState = Number(user.communityvisibilitystate);
+                    }
+
+                    if (!!user.profilestate) {
+                        Item.profileState = Number(user.profilestate);
+                    }
+
+                    if (!!user.lastlogoff) {
+                        Item.lastLogoff = Number(user.lastLogoff);
+                    }
+
+                    if (!!user.primaryclanid) {
+                        Item.primaryClanId = user.primaryclanid.toString();
+                    }
+
+                    if (!!user.loccountrycode) {
+                        Item = {
+                            ...Item,
+                            ...steamCountries.getLocation(user.loccountrycode, user.locstatecode, user.loccityid)
+                        };
+                    }
+
+                    if (!!user.timecreated) {
+                        Item.timeCreated = Number(user.timecreated);
+                    }
+
+                    Item.lastModified = Math.floor(Date.now() / 1000);
+
+                    return {
+                        PutRequest: {
+                            Item
+                        }
+                    }
+                })
+            }
+        });
+    }
+
+    /** Store into DynamoDB */
+    const ret = await Promise.all(ddbBatchWrites.map((params) => {
+        return new Promise((resolve, reject) => {
+            return DocClient.batchWrite(params, (err, data) => {
+                if (err) {
+                    console.error("Error batch writing:", err);
+                    return reject(err);
+                }
+
+                console.log("batchWrite data:", data);
+                return resolve(data);
             });
-        } else {
-            console.info("Finished daily requests:", requestCount);
-            return redisController.disconnect();
-        }
-    });
+        })
+    }));
+
+    await setParameter("localAccountId", localAccountId);
+    return ret;
 }
 
-/*
+/**
  * Generates a SteamID from an AccountID
- * @param accountId number
- * @returns steamid string
+ * @param  {number} accountId - 32 but unsigned integer accountId
+ * @returns {string} steamid - 64 bit unsigned integer converted to string
  */
 function generateSteamId(accountId) {
     /*
-     * SteamID Format:
-     *   8 bit universe, 4 bit account type, 20 bit instance, 32 bit account id
-     *   First 32 bits: 0x01100001
+     * steamId Format:      64 bit unsigned integer converted to string
+     * steamID Composition: [ 0x01100001, accountId ]
+     * Explanation:         [ 8 bit universe, 4 bit account type, 20 bit instance, 32 bit accountId ]
      *
      * Source: https://developer.valvesoftware.com/wiki/SteamID#Format
      */
@@ -92,14 +152,127 @@ function generateSteamId(accountId) {
     return buf.readBigUInt64BE(0).toString();
 }
 
-exports.handler = async(event, context) => {
-    const startAccountId = await redisController.getCurrentAccountId(BATCH_SIZE);
+/**
+ * Races a promise with a timeout. Several operations can hang on failure and this prevents the entire Lambda function from hanging
+ * @param  {Promise} promise - Promise to race
+ * @param  {number} timeout - Milliseconds to wait before timing out
+ * @param  {string} name="racePromise" - Used for logging the operation that failed
+ * @returns {promise}
+ */
+function racePromise(promise, timeout, name = "racePromise") {
+    let opTimeout;
 
-    // Check if crawler has finished
-    if (startAccountId >= END_ACCOUNT_ID) {
-        console.warn("Crawler has reached AccountId limit!");
-        return Promise.resolve("done");
+    return Promise.race([
+        promise.finally(() => {
+            if (!!opTimeout) {
+                clearTimeout(opTimeout)
+            }
+        }),
+        new Promise((resolve, reject) => {
+            opTimeout = setTimeout(() => {
+                console.warn(`${name} timeout!`);
+                return reject(`${name} timeout!`);
+            }, timeout);
+        })
+    ])
+}
+
+/**
+ * Retrieves the crawler state from DynamoDB
+ * @param  {string} parameterName
+ * @returns {number}
+ */
+function getParameter(parameterName) {
+    return racePromise(new Promise((resolve, reject) => {
+        return DocClient.get({
+            TableName: "parameters",
+            Key: {
+                parameterName
+            },
+            ProjectionExpression: "parameterValue"
+        }, (err, data) => {
+            if (err) {
+                console.error("getParameter", parameterName, err);
+                return reject(err);
+            }
+
+            console.log("getParameter success", parameterName, data.Item.parameterValue);
+            return resolve(data.Item.parameterValue);
+        });
+    }), 5000, `getParameter ${parameterName}`);
+}
+
+/**
+ * Saves the state of the Lambda function for the next invokation
+ * @param  {string} parameterName
+ * @param  {number} parameterValue
+ * @returns {number}
+ */
+function setParameter(parameterName, parameterValue) {
+    return racePromise(new Promise((resolve, reject) => {
+        return DocClient.put({
+            TableName: "parameters",
+            Item: {
+                parameterName,
+                parameterValue
+            }
+        }, (err, data) => {
+            if (!!err) {
+                console.error("setParameter error:", err);
+                return reject(err);
+            }
+
+            console.log("setParameter success", parameterName, data);
+            return resolve(data);
+        });
+    }), 5000, `setParameter ${parameterName}`);
+}
+
+/**
+ * Recursively requests steamIds from Steam API until limit is reached, then saves the current state in the DB
+ * @param  {number} currentAccountId
+ * @returns {Promise}
+ */
+async function crawlSteamIds(currentAccountId) {
+    await getSteamIds(currentAccountId, BATCH_SIZE);
+
+    if (++requestCount < REQUESTS_PER_INVOCATION) {
+        return crawlSteamIds(currentAccountId + BATCH_SIZE);
     }
+}
 
-    return processSequentially(startAccountId);
+/**
+ * Lambda function's entrypoint
+ * @param  {object} event
+ * @param  {object} context
+ * @returns {Promise}
+ */
+exports.handler = async(event, context) => {
+    console.log("Cralwer invoked");
+
+    try {
+        const startAccountId = await getParameter("currentAccountId");
+
+        // Check if crawler has finished
+        if (startAccountId >= END_ACCOUNT_ID) {
+            console.warn("Crawler has reached AccountId limit!");
+            return Promise.reject("done");
+        }
+
+        // Run crawler
+        await crawlSteamIds(startAccountId);
+
+        // Store state
+        return setParameter("currentAccountId", startAccountId + ((requestCount + 1) * BATCH_SIZE));
+    } catch (err) {
+        return Promise.reject(err);
+    } finally {
+        // Reset internal Lambda state
+        requestCount = 0;
+    }
 };
+
+// Run the Lambda function's entry point on a dev execution
+if (!IS_LAMBDA_ENVIRONMENT) {
+    return this.handler();
+}
